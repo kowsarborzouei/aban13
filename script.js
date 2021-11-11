923362f8d7679ea7446a91e1bd3c2315e8351f6e
kowsar <k.borzuei@gmail.com>
2021-11-11 18:37:09 +0330

    folder image

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ function Movie(title, description, rate, img, date, time) {
 }
 
 const moviesInfo = [];
-moviesInfo.push(new Movie('avengers:Endgame', 'Lorem ipsum.', 8.8, 'img.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
-moviesInfo.push(new Movie('Hobbs & show', 'Lorem ipsum.', 9.2, 'img_1.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
-moviesInfo.push(new Movie('John Wick 3', 'Lorem ipsum.', 8.8, 'img_3.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
-moviesInfo.push(new Movie('Deadpool 2', 'Lorem ipsum.', 8.1, 'img_2.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
-moviesInfo.push(new Movie('The Lion King', 'Lorem ipsum.', 7.4, 'img_4.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
-moviesInfo.push(new Movie('Mad Max: Fury Road', 'Lorem ipsum.', 8.1, 'img_5.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
-moviesInfo.push(new Movie('Aquaman', 'Lorem ipsum.', 9, 'img_6.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
-moviesInfo.push(new Movie('Mission Impossible', 'Lorem ipsum.', 7.8, 'img_7.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('avengers:Endgame', 'Lorem ipsum.', 8.8, '../image/img.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('Hobbs & show', 'Lorem ipsum.', 9.2, '../image/img_1.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('John Wick 3', 'Lorem ipsum.', 8.8, '../image/img_3.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('Deadpool 2', 'Lorem ipsum.', 8.1, '../image/img_2.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('The Lion King', 'Lorem ipsum.', 7.4, '../image/img_4.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('Mad Max: Fury Road', 'Lorem ipsum.', 8.1, '../image/img_5.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('Aquaman', 'Lorem ipsum.', 9, '../image/img_6.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
+moviesInfo.push(new Movie('Mission Impossible', 'Lorem ipsum.', 7.8, '../image/img_7.png', 'TUE 4 JULY_05:00PM', '2HR 13MIN'))
 
 const cardView = (cardInfo) => {
     return ` 
