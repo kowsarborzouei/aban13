8eaf5809aecc197e93a35fd1b18bd5f5fa7bd18b
kowsar <k.borzuei@gmail.com>
2021-11-11 12:14:07 +0330

    edit

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 
-function Movie(title,description,rate,img){
+function Movie(title,description,img){
     this.title=title;
     this.description=description;
     this.rate=rate;
     this.img=img;
 }
 const  moviesInfo=[];
-moviesInfo.push(new Movie('avengers','',8.7,''))
-moviesInfo.push(new Movie('avengers','',8.7,''))
-moviesInfo.push(new Movie('avengers','',8.7,''))
-moviesInfo.push(new Movie('avengers','',8.7,''))
+moviesInfo.push(new Movie('avengers','good','img.png'))
+moviesInfo.push(new Movie('css','nice','img.png'))
+moviesInfo.push(new Movie('html','exelent','img.png'))
+moviesInfo.push(new Movie('react','hard','img.png'))
+
+const  generateCards=(cardInfoList)=>{
+    return  cardInfoList.map(cardInfo=>{
+        return cardView(cardInfo)
+    });}
+
+const  cardContainer= document.getElementById('container')
+cardContainer.innerHTML=generateCards(moviesInfo)
 
 
 const cardView=(cardInfo)=>{
-return` <div id="card" >
-        <img src="img.png">
-    </div>`
+return` 
+    <div class="card" >
+        <img class="photo" src=${cardInfo.img}/>
+         <div>
+            <h3 class="title">${cardInfo.title}</h3>
+            <p class="dec">${cardInfo.description}</p>
+        </div>
+    </div>
+`
 }
-
-const  generateCards=(cardInfoList)=>{
-   return  cardInfoList.map(cardInfo=>{
-        return cardView(cardInfo)
-    });}
-const  cardContainer= document.getElementById('container')
-cardContainer.innerHTML=generateCards(moviesInfo)
 
 
 function  onSearch(element){
